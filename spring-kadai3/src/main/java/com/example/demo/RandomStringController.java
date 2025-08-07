ce0053e74f505a62182d3ba99a653f00cf6571f5
genkichi <genshigoto0821@gmail.com>
2025-08-07 12:38:49 +0900

    問題4 英字を含めるチェックボックスの追加

--- spring-kadai3/src/main/java/com/example/demo/RandomStringController.java
+++ spring-kadai3/src/main/java/com/example/demo/RandomStringController.java
@@ -14,7 +14,9 @@ import org.springframework.web.servlet.ModelAndView;
 //       テキストボックス画からの場合はデフォルト値として１個生成するようにする。
 //----------------------------------------------------------------------
 
-
+// ----------------------------------------------------------------------
+// [概要] 問題4 ランダム文字列生成時、画面から「数字を含める」「英字を含める」を指定できるようにしてください。両方にチェックが入っていない場合は文字列の生成を行いません。
+// 
 // コントローラの作成
 @Controller
 public class RandomStringController {
@@ -33,11 +35,21 @@ public class RandomStringController {
 	public ModelAndView generate(@RequestParam("length") int length,
 	@RequestParam(name = "includeNumber", defaultValue = "") String includeNumber,
 	@RequestParam(name= "count", defaultValue = "1") int count,
+	@RequestParam(name= "includeAlpha", defaultValue = "") String includeAlpha, // 英字を含めるチェックボックス追加による。
 	ModelAndView mv) {
 //		includeNumberの値が1であればtrueを返す変数
 		boolean includeNumberFlag = "1".equals(includeNumber);
+//		includeAlphaのチェック判定用の変数
+		boolean includeAlphaFlag = "1".equals(includeAlpha);
+//		チェックボックスのどちらにもチェックが入っていない場合の処理
+		if (!includeNumberFlag && !includeAlphaFlag) {
+			mv.addObject("error", "英字または数字のどちらかは必須です。");
+			mv.setViewName("random");
+			return mv;
+		}
+		
 //		Listクラスの変数results RandomStringGeneratorクラスのgenerateメソッドに引数を入れて値を取得する。(文字数、個数、true or false)
-		List<String> results = gen.generate(length, count, includeNumberFlag);
+		List<String> results = gen.generate(length, count, includeNumberFlag, includeAlphaFlag); // includeAlphaFlagを追加。
 //	ModelAndViewのresultsに変数resultsの値を格納
 		mv.addObject("results", results);
 //	ModelAndViewの値をrandom.htmlにセットする。
--- spring-kadai3/src/main/java/com/example/demo/RandomStringGenerator.java
+++ spring-kadai3/src/main/java/com/example/demo/RandomStringGenerator.java
@@ -10,21 +10,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class RandomStringGenerator {
 	// 引数にint,int,booleanを受取、List型の返り値を返すgenerateメソッド
-	public List<String> generate(int length, int count, boolean includeNumbers) {
+	public List<String> generate(int length, int count, boolean includeNumbers, boolean includeAlpha) {
 		// リストの数が決まっていないリストとしてArrayListを作成。
 		List<String> list = new ArrayList<>();
 		
+		// includeNumbers、includeAlphaにfalseが渡された場合の処理。
+		if (!includeNumbers && !includeAlpha) {
+			// 空のリストを返す
+			return list;
+		}
 		
 //	countで指定された回数だけ次の処理を繰り返すforの作成
 		for(int i = 0; i < count; i++) {
 			// 条件分岐 inculudeNumbersの値がtrueであれば以下の処理を実行
-			if (includeNumbers) {
-				// RandomStringUtilsライブラリを使用してランダムな文字列を取得（数字入り） 前提条件としてcommons-lang3の依存関係を設定。(build.gradle)
-				list.add(RandomStringUtils.randomAlphanumeric(length));
-				// falseの場合ランダムな文字列（数字なし）を追加。
-			} else {
-				list.add(RandomStringUtils.randomAlphabetic(length));
-			}
+//			if (includeNumbers) {
+//				// RandomStringUtilsライブラリを使用してランダムな文字列を取得（数字入り） 前提条件としてcommons-lang3の依存関係を設定。(build.gradle)
+//				list.add(RandomStringUtils.randomAlphanumeric(length));
+//				// falseの場合ランダムな文字列（数字なし）を追加。
+//			} else {
+//				list.add(RandomStringUtils.randomAlphabetic(length));
+//			}
+			
+//			RandomStringUtilsライブラリのランダムメソッドに変更
+//			変数strに引数に指定した値を元にランダムな文字列を自動作成、代入  random(int 文字数, boolean useLetters, boolean useNumbers)
+			String str = RandomStringUtils.random(length, includeAlpha, includeNumbers);
+			list.add(str);
 		}
 		// 戻り値としてlistを返す。
 		return list;
